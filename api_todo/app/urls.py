de21059321e4fed84c8b732a4f52dffbdde138ee
Anjel Diniz <anjeldiniz@gmail.com>
2023-01-11 00:45:12 -0300

    feat: refactor routes using ViewSets

--- api_todo/app/urls.py
+++ api_todo/app/urls.py
@@ -1,8 +1,7 @@
-from app.views import TodoListAndCreate, TodoDetailChangeAndDelete
+from app.views import TodoViewSet
 
-from django.urls import path
+from rest_framework.routers import DefaultRouter
 
-urlpatterns = [
-  path("", TodoListAndCreate.as_view()),
-  path("<int:pk>", TodoDetailChangeAndDelete.as_view()),
-]
+router = DefaultRouter()
+router.register(r"", TodoViewSet)
+urlpatterns = router.urls
