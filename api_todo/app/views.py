e0b3645df00e8f83614676a903cfb71c9a9c83b4
Anjel Diniz <anjeldiniz@gmail.com>
2023-01-10 23:56:53 -0300

    feat: Refactor route /todo in methods GET and POST

--- api_todo/app/views.py
+++ api_todo/app/views.py
@@ -4,19 +4,20 @@ from app.serializers import TodoSerializer
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.views import APIView
 
-@api_view(["GET", "POST"])
-def todo_list(request):
-  if request.method == "GET":
-    todo = Todo.objects.all()
-    serializer = TodoSerializer(todo, many=True)
-    return Response(serializer.data)
-  elif request.method == "POST":
-    serializer = TodoSerializer(data=request.data)
-    if serializer.is_valid():
-      serializer.save()
-      return Response(serializer.data, status=status.HTTP_201_CREATED)
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+class TodoListAndCreate(APIView):
+  def get(self, request):
+      todo = Todo.objects.all()
+      serializer = TodoSerializer(todo, many=True)
+      return Response(serializer.data)
+
+  def post(self, request):
+      serializer = TodoSerializer(data=request.data)
+      if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+      return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(["GET", "PUT", "DELETE"])
 def todo_detail_change_and_delete(request, pk):
