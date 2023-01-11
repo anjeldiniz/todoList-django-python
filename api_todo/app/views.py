10673a0afb104d44b979c7b2eab8c32bfa56bded
Anjel Diniz <anjeldiniz@gmail.com>
2023-01-11 00:17:02 -0300

    feat: refactor route /todo/pk in methods get, put and delete

--- api_todo/app/views.py
+++ api_todo/app/views.py
@@ -3,6 +3,7 @@ from app.serializers import TodoSerializer
 
 from rest_framework import status
 from rest_framework.decorators import api_view
+from rest_framework.exceptions import NotFound
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
@@ -19,22 +20,27 @@ class TodoListAndCreate(APIView):
         return Response(serializer.data, status=status.HTTP_201_CREATED)
       return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-@api_view(["GET", "PUT", "DELETE"])
-def todo_detail_change_and_delete(request, pk):
-  try:
-      todo = Todo.objects.get(pk=pk)
-  except Todo.DoesNotExist:
-    return Response(status=status.HTTP_404_NOT_FOUND)
+class TodoDetailChangeAndDelete(APIView):
+  def get_object(self, pk):
+    try:
+        return Todo.objects.get(pk=pk)
+    except Todo.DoesNotExist:
+      raise NotFound()
 
-  if request.method == "GET":
-    serializer = TodoSerializer(todo)
-    return Response(serializer.data)
-  elif request.method == "PUT":
-    serializer = TodoSerializer(todo, data=request.data)
-    if serializer.is_valid():
-      serializer.save()
+  def get(self, request, pk):
+      todo = self.get_object(pk)
+      serializer = TodoSerializer(todo)
       return Response(serializer.data)
-    return Response(serializer.data)
-  elif request.method == "DELETE":
+
+  def put(self, request, pk):
+      todo = self.get_object(pk)
+      serializer = TodoSerializer(todo, data=request.data)
+      if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+      return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+  def delete(self, request, pk):
+      todo = self.get_object(pk)
       todo.delete()
       return Response(status=status.HTTP_204_NO_CONTENT)
